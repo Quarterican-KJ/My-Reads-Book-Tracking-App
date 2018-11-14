598f250f2f66375c0e15182bab5e794075d5f44e
Steven Babcock <sbabcock20@gmail.com>
2018-11-14 16:43:10 -0600

    merger

--- src/Components/Shelf.js
+++ src/Components/Shelf.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import Book from './Book';
 
@@ -20,27 +19,3 @@ class Shelf extends React.Component {
 } 
 
 export default Shelf;
-||||||| merged common ancestors
-=======
-import React from 'react';
-import Book from './Book';
-
-class Shelf extends React.Component {
-   render() {
-      return ( 
-       <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.name}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {
-              this.props.book.map((book, key) => <Book bookUpdater={this.props.bookUpdater}  book={book} key={key} />)
-            }
-          </ol>
-        </div>
-      </div>
-      );
-   }
-} 
-
-export default Shelf;
->>>>>>> dd98caf4795c85d2d2727cadf8e3abe6251945ef
